6922ba57a40479cff64672b6879789801672b34d
Paridhilamichhane12 <paridhilamichhanee245@gmail.com>
2024-11-27 07:12:19 +0545

    initial commit

---
+++ Models/student.cs
@@ -0,0 +1,25 @@
+﻿namespace paridhi.Models
+{
+    public class student
+    {
+    0 references
+    public int Id { get; set; }
+    0 references
+    public string FirstName { get; set; }
+    0 references
+    public string LastName { get; set; }
+    0 references
+    public string Email { get; set; }
+    0 references
+    public string Phone { get; set; }
+     0 references
+     public string EnrollmentDate { get; set; }
+     0 references
+     public string Address { get; set; }
+     0 references
+     public string CreatedDate { get; set; }
+     0 references
+     public string CreatedBy { get; set; }
+    }
+
+      
